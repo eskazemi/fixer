59944f2dd19d464b5f7328d726d5b320ab3c6dda
Esmaeil kazemi <smk182018@gmail.com>
2022-11-05 12:24:41 +0330

    Feat(): initail commit

---
+++ build/lib/fixer/__init__.py
@@ -0,0 +1,4 @@
+from fixer.handler import get_rates
+
+
+__all__ = ["get_rates"]
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    version="0.0.1",
+    name="fixer-demo",
+    author="Esmaeil Kazemi",
+    author_email="m.esmaeilkazemi@gmail.com",
+    packages=["fixer"],
+    license="MIT",
+    url="#",
+    zip_safe=False,
+)
